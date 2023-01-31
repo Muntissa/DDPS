b191a6cc67e3f7c6b54f235a4035a17397949816
Muntissa <h3r1zma@gmail.com>
2023-01-31 10:21:24 +0300

    Добавьте файлы проекта.

---
+++ Api/Entities/Clients.cs
@@ -0,0 +1,12 @@
+﻿namespace DDPS.Api.Entities
+{
+    public class Clients
+    {
+        int Id { get; set; }
+        public string SecondName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
